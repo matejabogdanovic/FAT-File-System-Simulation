fd93c2e848000a364f589b58841e082564670eee
matejabodanovic <bogdanovicmateja75@gmail.com>
2024-12-24 14:06:10 +0100

    Console

---
+++ h/Console.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "DirectorySystem.h"
+
+class Console {
+public:
+    static int open();
+
+private:
+    static void printHelp();
+};
+
+
---
+++ src/Console.cpp
@@ -0,0 +1,30 @@
+#include "../h/Console.h"
+#include <iostream>
+
+// todo optimize with map?
+int Console::open() {
+
+    std::string command;
+    while(true) {
+        std::cout << DirectorySystem::getWorkingDirectoryName() << ">";
+
+        std::getline(std::cin, command);
+        if(command == "exit")
+            break;
+        else if(command == "tree") {
+            DirectorySystem::printTree();
+        } else if(command == "help pls") {
+            printHelp();
+        } else {
+            std::cout << "Unknown command. Please, type 'help pls' for help.\n";
+        }
+    }
+
+    return 0;
+}
+
+void Console::printHelp() {
+    std::cout << "No problem. Check out these sick commands!\n"
+              << "'exit' - quit console\n"
+              << "'tree' - list tree\n";
+}
